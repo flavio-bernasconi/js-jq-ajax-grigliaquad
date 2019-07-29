8c55438459238e874f0ff591dc74ddb88970465a
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-29 13:31:25 +0200

    ok

---
+++ main.js
@@ -0,0 +1,63 @@
+$( document ).ready(function() {
+
+
+     var source = "https://flynn.boolean.careers/exercises/api/random/int";
+
+
+     $(".box").one("click",
+       function(){
+         var questo = $(this);
+         $.ajax(
+            {
+               url : source,
+               method: "GET",
+               success: function(data){
+                  var numeroRandom = data.response;
+                  console.log(numeroRandom);
+
+                  if (numeroRandom >  5) {
+                    questo.css("background-color","green");
+                    questo.find("p").text(numeroRandom);
+                  }
+                  else {
+                    questo.css("background-color","yellow");
+                    questo.find("p").text(numeroRandom);
+                  }
+
+               },
+               error: function(richiesta,stato,errore){
+                  console.log("c'è un problema con il server",richiesta,stato,errore);
+               }
+            }
+         );
+       }
+     )
+
+
+
+
+
+
+
+
+//   $.ajax(
+//      {
+//         url : apirandnum,
+//         method: "GET",
+//         success: function(data){
+//          console.log(data);
+//          $(".randnum").text(data.response);
+//         },
+//         error: function(){
+//             console.log("c'è stato un errore riprova più tardi.")
+//         }
+//      }
+//   );
+
+//      console.log("io sono prepotente e voglio apparire prima!");
+
+
+
+
+
+});
